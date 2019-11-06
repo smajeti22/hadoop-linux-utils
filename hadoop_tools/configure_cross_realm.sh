2f9192e79702c05efdead9a2fe57003b1bdbdafe
Srinivasu Majeti <smajeti@smajeti.local>
2019-11-06 17:18:03 +0530

    fix empty string value for host

--- hadoop_tools/configure_cross_realm.sh
+++ hadoop_tools/configure_cross_realm.sh
@@ -19,7 +19,7 @@ CLUSTER1_AMBARI_HOST=$3
 #User Input Required Optional . Assumed to be same as first cluster/defaults if not given input
 CLUSTER1_KADMIN_PASSWORD=hadoop
 CLUSTER2_KADMIN_PASSWORD=hadoop
-if [ $CLUSTER1_AMBARI_HOST == "" ];
+if [ "$CLUSTER1_AMBARI_HOST" == "" ];
 then
 	CLUSTER1_AMBARI_HOST=localhost
 fi
@@ -38,26 +38,6 @@ CLUSTER1_NAME=""
 CLUSTER2_NAME=""
 CLUSTER1_DOMAIN=""
 CLUSTER2_DOMAIN=""
-
-#usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
-#[ $# -eq 0 ] && usage
-#while getopts ":hs:p:" arg; do
-#  case $arg in
-#    p) # Specify p value.
-#      echo "p is ${OPTARG}"
-#      ;;
-#    s) # Specify strength, either 45 or 90.
-#      strength=${OPTARG}
-#      [ $strength -eq 45 -o $strength -eq 90 ] \
-#        && echo "Strength is $strength." \
-#        || echo "Strength needs to be either 45 or 90, $strength found instead."
-#      ;;
-#    h | *) # Display help.
-#      usage
-#      exit 0
-#      ;;
-#  esac
-#done
 
 getMachineType()
 {
